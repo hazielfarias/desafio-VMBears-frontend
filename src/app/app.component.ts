2f8df5a0839e69928847a6bad0a8edba0372fd92
Haziel Farias <hazielfarias@gmail.com>
2023-01-12 02:07:24 -0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'desafio-front';
+  selectedFile: any = null;
+  fileContent: string | undefined;
+
+  loading = false;
+
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0] ?? null;
+
+  }
+
+  setFileContent(texto: string | undefined) {
+    this.fileContent = texto;
+  }
+
+  load() {
+    let fileReader: FileReader = new FileReader();
+    this.fileContent = '';
+    fileReader.onload = (e) => {
+      const text = fileReader.result?.toString().trim();
+      this.setFileContent(text);
+    }
+    fileReader.readAsText(this.selectedFile);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material.module';
+import { ApiService } from './service/api.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
